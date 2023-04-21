9d75feff552d82034fcddf723614e20e188572c2
solomonferede <solomonf1227@gmail.com>
2023-04-21 13:02:33 +0300

    create file /tmp/school

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,11 +1,9 @@
 # Create a file /tmp/school
-
-str = "I love Puppet"
 file { '/tmp/school':
-    ensure => 'file',
-    path => '/tmp/school
-    owner => 'www-data',
-    group => 'www-data',
-    mode => 0744,
-    content => $str
+    ensure  => 'file',
+    path    => '/tmp/school',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet'
 }

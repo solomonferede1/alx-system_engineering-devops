e3563ad7bad81b67078b6228ed38106954aca2e7
solomonferede <solomonf1227@gmail.com>
2023-04-21 13:46:01 +0300

    kill me now - killing aprocess

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # kill a process
 exec { 'killmenow':
-    path     => '/usr/bin:/usr/sbin:/bin',
+    path    => '/usr/bin:/usr/sbin:/bin',
     command => 'pkill killmenow',
-    onlyif => '/usr/bin/pgrep killmenow'
+    onlyif  => '/usr/bin/pgrep killmenow'
 }

78a7e916b687f0249329bf7308162959d90ff4ee
solomonferede <solomonf1227@gmail.com>
2023-04-21 12:28:37 +0300

    I delete to do it again

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,5 +0,0 @@
-# install flask using pip3
-
-exec { 'flask':
-  command => '/usr/bin/apt pip3 install flask -v 2.1.0',
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,5 +0,0 @@
-# Create a manifest that kills a proccess named killmenow
-
-exec {'killmenow':
-command => '/usr/bin/pkill --full killmenow'
-    }

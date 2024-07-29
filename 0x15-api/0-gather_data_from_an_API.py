f4bf61dae076b365bc55b96c8c79b42a88a1eb79
Solomon <solomonf1227@gmail.com>
2024-07-29 15:49:03 +0300

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,22 +12,26 @@ def todo_list_completed(employee_id):
     url_todo = "https://jsonplaceholder.typicode.com/todos/"
 
     parms = {'id': employee_id}
-    user_data = requests.get(url_user, params=parms).json()
-    EMPLOYEE_NAME = user_data['name']
+    response = requests.get(url_user, params=parms).json()
+    for i in response:
+        if i['id'] == employee_id:
+            EMPLOYEE_NAME = i['name']
 
     parms = {'userId': employee_id}
-    response = requests.get(url_todo, params=parms)
-    todos = response.json()
+    response = requests.get(url_todo, params=parms).json()
 
-    completed = [task['title'] for task in todos if task['completed']]
+    completed = []
+    for todos in response:
+        if todos['completed']:
+            completed.append(todos['title'])
 
     NUMBER_OF_DONE_TASKS = len(completed)
-    NUMBER_OF_DONE_TASKS = len(todos)
+    NUMBER_OF_DONE_TASKS = len(response)
 
     print(f"Employee {EMPLOYEE_NAME} is done with tasks(\
-          {NUMBER_OF_DONE_TASKS}/{NUMBER_OF_DONE_TASKS}): ")
+{NUMBER_OF_DONE_TASKS}/{NUMBER_OF_DONE_TASKS}): ")
     for task in completed:
-        print(f"\t {task['title']}")
+        print(f"\t {task}")
 
 
 if __name__ == '__main__':

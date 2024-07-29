c4c90b15527a5ddb831e612dcf994c7e0ff80dc7
solomonferede1 <solomonf1227@gmail.com>
2024-07-29 11:41:27 -0700

    To json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -2,6 +2,7 @@
 
 """ Python script to export data in the JSON format."""
 
+import json
 import requests
 import sys
 
@@ -15,21 +16,23 @@ def todo_to_json(employee_id):
     response = requests.get(url_user, params=parms).json()
     for i in response:
         if i['id'] == employee_id:
-            EMPLOYEE_NAME = i['name']
             USERNAME = i['username']
 
     parms = {'userId': employee_id}
     response = requests.get(url_todo, params=parms).json()
 
-    line = []
+    todo_list = []
     for todos in response:
-        line.append(f'{{"task": "{todos["title"]}", "completed": {todos["completed"]}, "username": "{USERNAME}"}}')
-    
-    lines = f'{{"{employee_id}": {line}}}'
+        todo_list.append({
+            "task": todos["title"],
+            "completed": todos["completed"],
+            "username": USERNAME
+            })
+    data = {str(employee_id): todo_list}
 
     file_name = f'{employee_id}.json'
     with open(file_name, 'w') as file:
-        file.write(lines)
+        json.dump(data, file)
 
 
 if __name__ == '__main__':

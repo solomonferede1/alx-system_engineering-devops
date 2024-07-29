32e50588e7ed48bb50fce888b21d7c2ca15a5cdf
Solomon <solomonf1227@gmail.com>
2024-07-29 16:29:54 +0300

    Export to csv file format

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''Python script that, for a given employee ID,
+export to  CSV file'''
+
+import requests
+import sys
+
+
+def todo_list_completed(employee_id):
+    '''List completed tasks'''
+    url_user = "https://jsonplaceholder.typicode.com/users/"
+    url_todo = "https://jsonplaceholder.typicode.com/todos/"
+
+    parms = {'id': employee_id}
+    response = requests.get(url_user, params=parms).json()
+    for i in response:
+        if i['id'] == employee_id:
+            USERNAME = i['username']
+
+    parms = {'userId': employee_id}
+    response = requests.get(url_todo, params=parms).json()
+
+    file_name = f'{employee_id}.csv'
+    for todos in response:
+        line = f'"{employee_id}", "{USERNAME}",\
+            "{todos["completed"]}", "{todos["title"]}"\n'
+        with open(file_name, 'a') as file:
+            file.write(line)
+
+
+if __name__ == '__main__':
+    '''Call if main'''
+    employee_id = int(sys.argv[1])
+    todo_list_completed(employee_id)

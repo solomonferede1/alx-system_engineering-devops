1a96b99e1b5cb116ce380c7a08e4f8988383444c
solomonferede <solomonf1227@gmail.com>
2024-08-20 11:07:10 +0000

    format

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,7 +11,7 @@ def number_of_subscribers(subreddit):
     Function that queries the Reddit API
     - If not a valid subreddit, return 0.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'custom'}
     try:
         req = requests.get(url, headers=headers, allow_redirects=False)
